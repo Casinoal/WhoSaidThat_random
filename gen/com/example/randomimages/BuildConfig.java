01f06ae8e5699a887c13b4ce13f2ca2ce2611613
CasinoAl <alexwhyatt@gmail.com>
2015-01-11 23:42:30 +0200

    Initial Upload
    got the images working randomly

---
+++ gen/com/example/randomimages/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.randomimages;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/randomimages/ImageSoundItem.java
@@ -0,0 +1,46 @@
+package com.example.randomimages;
+
+public class ImageSoundItem {
+	private String image;
+	private String sound;
+	private int index;
+
+	ImageSoundItem() {
+
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	ImageSoundItem(String imageName, String soundName) {
+		this.image = imageName;
+		this.sound = soundName;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getSound() {
+		return sound;
+	}
+
+	public void setSound(String sound) {
+		this.sound = sound;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + image + ", " + sound + "]";
+	}
+
+}
